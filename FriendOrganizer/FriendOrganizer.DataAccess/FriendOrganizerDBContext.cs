ccb86be2f94c03803d00d6cc9aa2e6be256d0db6
Wiki019 <waqas.ahmad.019@outlook.com>
2020-04-10 16:13:31 +0200

    Add DbContext and Connection String

---
+++ FriendOrganizer/FriendOrganizer.DataAccess/FriendOrganizerDBContext.cs
@@ -0,0 +1,22 @@
+﻿using FriendOrganizer.model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FriendOrganizer.DataAccess
+{
+    class FriendOrganizerDBContext : DbContext
+    {
+        public FriendOrganizerDBContext() : base("FriendOrganizerDb")
+        {
+
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
