d3ee8da39586273b2f3e01fafcb5222cc67c3e7c
Wiki019 <waqas.ahmad.019@outlook.com>
2020-04-10 13:09:17 +0200

    Setup Autofac

---
+++ FriendOrganizer/FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.Startup
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<MainWindow>().AsSelf();
+
+            return builder.Build();
+        }
+    }
+}
